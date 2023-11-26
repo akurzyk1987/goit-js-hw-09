96f24b40033ba65ca41db4298da1a56e8282bf00
akurzyk1987 <134443000+akurzyk1987@users.noreply.github.com>
2023-11-26 15:14:41 +0100

    goit-js-hw-09 - Commit 3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
+
+startBtn.addEventListener('click', e => {
+  e.target.disabled = true;
+  timerId = setInterval(() => {
+    const backgroundColor = getRandomHexColor();
+    document.body.style.backgroundColor = backgroundColor;
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', e => {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+});
