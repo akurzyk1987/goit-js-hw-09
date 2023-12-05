eee3ecc9e2a6cb8a5c9a99c129dffddc2f46d981
akurzyk1987 <134443000+akurzyk1987@users.noreply.github.com>
2023-12-05 14:01:26 +0100

    goit-js-hw-09 - Commit 6

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import Notiflix from 'notiflix';
+
+const button = document.querySelector('button');
+button.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const delay = document.querySelector('[name="delay"]');
+  const step = document.querySelector('[name="step"]');
+  const amount = document.querySelector('[name="amount"]');
+
+  generatePromises(amount, delay, step);
+});
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function generatePromises(amount, delay, step) {
+  for (let i = 0; i < amount; i++) {
+    const currentDelay = delay + i * step;
+
+    createPromise(i + 1, currentDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
 }
